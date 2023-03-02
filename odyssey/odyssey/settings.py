12cde09669f276bfe047d221a9e5da2a4379a7a2
julietteceb16 <74938553+julietteceb16@users.noreply.github.com>
2023-03-02 10:43:49 -0600

    host

--- odyssey/odyssey/settings.py
+++ odyssey/odyssey/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@8@iz5b+uuqqvkv@j4*o_evd%^lpw$)foax0gb%1!x#0en%a#d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
